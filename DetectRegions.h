ea8be25ee0f7ebd06591475d7a2c28561d03ef21
wuyy <wuyongyu13@126.com>
2019-02-28 11:47:05 +0800

    车牌识别

---
+++ DetectRegions.h
@@ -0,0 +1,34 @@
+/*****************************************************************************
+*   Number Plate Recognition using SVM and Neural Networks
+*/
+
+#ifndef DetectRegions_h
+#define DetectRegions_h
+
+#include <string.h>
+#include <vector>
+
+#include "Plate.h"
+
+#include <cv.h>
+#include <highgui.h>
+#include <cvaux.h>
+
+using namespace std;
+using namespace cv;
+
+class DetectRegions{
+    public:
+        DetectRegions();
+        string filename;
+        void setFilename(string f);
+        bool saveRegions;
+        bool showSteps;
+        vector<Plate> run(Mat input);
+    private:
+        vector<Plate> segment(Mat input);
+        bool verifySizes(RotatedRect mr);
+        Mat histeq(Mat in);
+};
+
+#endif
